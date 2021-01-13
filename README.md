a0706505e19a371ca6fc02ea6322e91ecc0e7224
Hanz Badua <snow@snowyishere.tech>
2021-01-13 11:23:34 -0600

    Moved all projects up by 1 directory, stopped including Steam4NET source project with the repo. Furthermore, including a much smaller Steam4NET dll with optimizations enabled

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 ## This is a fork of SingleBoostr found in HourBoostr - you can find the original author's repository [here](https://github.com/Ezzpify/HourBoostr)
 ### This fork aims for simplicity (console-only interface, hopefully low RAM usage)
 ### If you have any problems with this fork, make an issue in this repository, NOT the original repository.
-#### This repository uses the Steam4NET library found [here](https://github.com/SteamRE/open-steamworks/tree/master/Steam4NET2/Steam4NET2)
 #### INIParser used for the config system is found [here](https://github.com/rickyah/ini-parser)
 #### The only source code used from the original repository is [this file](https://github.com/Ezzpify/HourBoostr/blob/master/SingleBoostr/SingleBoostrGame/SingleBoostrGame/Program.cs)
+#### Furthermore, this fork is licensed under the same license as the original repository: [GNU GPL-v3.0](https://github.com/Hxxzii/HourBoostr/blob/master/LICENSE)
+----
+Build instructions:
+(Optional) Build the Steam4NET library found [here](https://github.com/SteamRE/Steam4NET), and add as a reference to `SingleBoostr.IdlingProcess` - this is optional as this repository already comes with a compiled version of Steam4NET
+
+Compile with Visual Studio 2019 (NET Framework 4.8) required
+
+Afterwards, copy the compiled `SingleBoostr.IdlingProcess` binaries and put it in the same folder/directory with the `SingleBoostr.Client` binaries
+
+And that's it!
---
+++ SingleBoostr.IdlingProcess/Program.cs
@@ -0,0 +1,110 @@
+// TODO: Actually handle the errors+error codes
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Steam4NET;
+
+namespace SingleBoostr.IdlingProcess
+{
+    public class Program
+    {
+        private static ISteamClient017 _steamClient;
+        private static ISteamApps006 _steamApp;
+        private static BackgroundWorker _bwg;
+
+        private static ErrorCodes ConnectToSteam()
+        {
+            if (!Steamworks.Load(true))
+            {
+                return ErrorCodes.SteamworksFail;
+            }
+
+            _steamClient = Steamworks.CreateInterface<ISteamClient017>();
+            if (_steamClient == null)
+            {
+                return ErrorCodes.ClientFail;
+            }
+
+            int pipe = _steamClient.CreateSteamPipe();
+            if (pipe == 0)
+            {
+                return ErrorCodes.PipeFail;
+            }
+
+            int user = _steamClient.ConnectToGlobalUser(pipe);
+            if (user == 0)
+            {
+                return ErrorCodes.UserFail;
+            }
+
+            _steamApp = _steamClient.GetISteamApps<ISteamApps006>(user, pipe);
+            if (_steamApp == null)
+            {
+                return ErrorCodes.AppsFail;
+            }
+
+            return ErrorCodes.Success;
+        }
+
+        private static int Main(string[] args)
+        {
+            if (!uint.TryParse(args[0], out _))
+            {
+                var kill = Process.GetCurrentProcess();
+                kill.Close();
+                kill.Kill();
+                kill.WaitForExit();
+                return 1;
+            }
+
+            _bwg = new BackgroundWorker { WorkerSupportsCancellation = true };
+            _bwg.DoWork += async (sender, e) =>
+            {
+                var processList = Process.GetProcesses();
+                var parentProcess = processList.FirstOrDefault(o => o.Id == (int) e.Argument);
+
+                if (parentProcess == null)
+                {
+                    await Task.Delay(10000);
+                }
+                else
+                {
+                    while (!_bwg.CancellationPending)
+                    {
+                        if (parentProcess.HasExited)
+                            break;
+
+                        await Task.Delay(10000);
+                    }
+                }
+
+                Environment.Exit(1);
+            };
+
+            Environment.SetEnvironmentVariable("SteamAppId", args[0]);
+
+            if (ConnectToSteam() == ErrorCodes.Success)
+            {
+                if (args.Length >= 2 && int.TryParse(args[1], out var parentProcessId) && parentProcessId != 0)
+                    _bwg.RunWorkerAsync(parentProcessId);
+                Process.GetCurrentProcess().WaitForExit();
+                //await Task.Delay(-1);
+            }
+
+            return 0; 
+        }
+    }
+
+    
+    internal enum ErrorCodes : byte
+    {
+        Success = 0, 
+        SteamworksFail = 1,
+        ClientFail = 2,
+        PipeFail = 3,
+        UserFail = 4,
+        AppsFail = 5
+    }
+}
