289a0009bd44c90ec8330a5ef824f6e857461136
Hanz Badua <snow@snowyishere.tech>
2021-01-12 18:34:50 -0600

    Initial commit of this fork

---
+++ SingleBoostr/SingleBoostr.Client/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using IniParser;
+using IniParser.Model;
+
+namespace SingleBoostr.Client
+{
+    public class Program 
+    {
+        private static async Task<int> Main()
+        {
+            var applist = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "applist.txt");
+            var exe = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SingleBoostr.IdlingProcess.exe");
+            var config = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.ini");
+
+            if (!File.Exists(exe))
+            {
+                Console.WriteLine("Helper exe not located, please put this program beside the helper exe or reinstall this program");
+                await Task.Delay(-1);
+                return 1;
+            }
+
+            if (!File.Exists(applist))
+            {
+                File.CreateText(applist).Dispose();
+                Console.WriteLine("App list doesn't exist (applist.txt)");
+                Console.WriteLine("App list file has been created, please add your appids that you want to idle in the config file");
+                Console.WriteLine("(exit this program, then edit the config file)");
+                await Task.Delay(-1);
+                return 1;
+            }
+
+            if (!File.Exists(config))
+            {
+                File.CreateText(config).Dispose();
+                Console.WriteLine("Config doesn't exist (config.ini)");
+                Console.WriteLine("Config file has been created, please adjust accordingly");
+                
+                var _configParser = new FileIniDataParser();
+                var _configData = new IniData();
+                _configData.Sections.AddSection("Config");
+                _configData["Config"].AddKey("SecondsUntilRestartAll", "3600");
+                _configParser.WriteFile(config, _configData);
+                
+                Console.WriteLine("(exit this program, then edit the config file)");
+                await Task.Delay(-1);
+                return 1;
+            }
+
+            var configParser = new FileIniDataParser();
+            var configData = configParser.ReadFile(config);
+
+            if (int.TryParse(configData["Config"]["SecondsUntilRestartAll"], out var seconds))
+            {
+                // If config fails, set the default reset interval to 1 hour
+                if (seconds <= 0)
+                {
+                    Console.WriteLine("SecondsUntilRestartAll is zero or a negative number - defaulting to 3600 seconds (1 hour)");
+                    seconds = 3600;
+                }
+                
+                Console.WriteLine($"Idling processes will restart every {seconds} seconds");
+            }
+            
+            while (true)
+            {
+                foreach (var i in File.ReadAllLines(applist))
+                {
+                    if (!int.TryParse(i, out _) || string.IsNullOrWhiteSpace(i))
+                    {
+                        Console.WriteLine($"AppId {i} is an invalid AppId, skipping");
+                        continue;
+                    }
+
+                    var startInfo = new ProcessStartInfo(exe)
+                    {
+                        UseShellExecute = false,
+                        WindowStyle = ProcessWindowStyle.Normal,
+                        CreateNoWindow = true,
+                        //RedirectStandardInput = true,
+                        //RedirectStandardOutput = true,
+                        //RedirectStandardError = true,
+                        Arguments = $"{i} {Process.GetCurrentProcess().Id}"
+                    };
+
+                    var startProcess = Process.Start(startInfo);
+                    IdlingProcesses.Add(startProcess);
+                    Console.WriteLine($"AppId {i} is now boosting!");
+                }
+                
+                await Task.Delay(seconds*1000);
+                
+                foreach (var i in IdlingProcesses)
+                {
+                    i.Kill();
+                    i.Close();
+                }
+                
+                IdlingProcesses.Clear();
+                Console.WriteLine("All idling processes have been restarted");
+            }
+        }
+
+        private static List<Process> IdlingProcesses { get; } = new List<Process>();
+    }
+}
---
+++ SingleBoostr/SingleBoostr.IdlingProcess/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Steam4NET;
+
+namespace SingleBoostr.IdlingProcess
+{
+    public class Program
+    {
+        private static ISteamClient017 _steamClient012;
+        private static ISteamApps006 _steamApps001;
+        private static BackgroundWorker _bwg;
+
+        private static ErrorCodes ConnectToSteam()
+        {
+            if (!Steamworks.Load(true))
+            {
+                return ErrorCodes.SteamworksFail;
+            }
+
+            _steamClient012 = Steamworks.CreateInterface<ISteamClient017>();
+            if (_steamClient012 == null)
+            {
+                return ErrorCodes.ClientFail;
+            }
+
+            int pipe = _steamClient012.CreateSteamPipe();
+            if (pipe == 0)
+            {
+                return ErrorCodes.PipeFail;
+            }
+
+            int user = _steamClient012.ConnectToGlobalUser(pipe);
+            if (user == 0)
+            {
+                return ErrorCodes.UserFail;
+            }
+
+            _steamApps001 = _steamClient012.GetISteamApps<ISteamApps006>(user, pipe);
+            if (_steamApps001 == null)
+            {
+                return ErrorCodes.AppsFail;
+            }
+
+            return ErrorCodes.Success;
+        }
+
+        private static async Task<int> Main(string[] args)
+        {
+            if (!uint.TryParse(args[0], out _))
+            {
+                var kill = Process.GetCurrentProcess();
+                kill.Close();
+                kill.Kill();
+                kill.WaitForExit();
+                return 1;
+            }
+
+            _bwg = new BackgroundWorker { WorkerSupportsCancellation = true };
+            _bwg.DoWork += async (sender, e) =>
+            {
+                var processList = Process.GetProcesses();
+                var parentProcess = processList.FirstOrDefault(o => o.Id == (int) e.Argument);
+
+                if (parentProcess == null)
+                {
+                    await Task.Delay(10000);
+                }
+                else
+                {
+                    while (!_bwg.CancellationPending)
+                    {
+                        if (parentProcess.HasExited)
+                            break;
+
+                        await Task.Delay(10000);
+                    }
+                }
+
+                Environment.Exit(1);
+            };
+
+            Environment.SetEnvironmentVariable("SteamAppId", args[0]);
+
+            if (ConnectToSteam() == ErrorCodes.Success)
+            {
+                if (args.Length >= 2 && int.TryParse(args[1], out var parentProcessId) && parentProcessId != 0)
+                    _bwg.RunWorkerAsync(parentProcessId);
+                Process.GetCurrentProcess().WaitForExit();
+                //await Task.Delay(-1);
+            }
+
+            return 0; 
+        }
+    }
+
+    internal enum ErrorCodes : byte
+    {
+        Success = 0, 
+        SteamworksFail = 1,
+        ClientFail = 2,
+        PipeFail = 3,
+        UserFail = 4,
+        AppsFail = 5
+    }
+}
