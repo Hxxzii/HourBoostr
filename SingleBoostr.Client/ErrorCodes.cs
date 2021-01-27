589f8bf882d3ef4c8743efeeb16de8ccc00215cc
Hanz Badua <snow@snowyishere.tech>
2021-01-27 11:01:14 -0600

    pipefail error discovery 2/2

--- SingleBoostr.Client/ErrorCodes.cs
+++ SingleBoostr.Client/ErrorCodes.cs
@@ -5,7 +5,7 @@
         Success = 0, // Success
         SteamworksFail = 1, // Steam client is NOT open
         ClientFail = 2, // Unknown
-        PipeFail = 3, // Unknown
+        PipeFail = 3, // Related to SteamworksFail, needs more testing
         UserFail = 4, // User doesn't own the app/appId
         AppsFail = 5, // Unknown
         InvalidArguments = 10, // Process called with invalid arguments - should only be returned when user tries to open background exe without the client exe
